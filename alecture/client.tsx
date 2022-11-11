f002af044c61d5dfcd0743bf16ce830bd4068e5f
Eunseo Choi <yses9296@gmail.com>
2022-11-12 02:01:04 +0900

    Slack_Clone client setting

--- alecture/client.tsx
+++ alecture/client.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from '@layouts/App';
 
-
-import App from './layouts/App'
-
-render(<App />, document.querySelector('#root'));
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.querySelector('#app'),
+);
