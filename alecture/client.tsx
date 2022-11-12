d920d59368f142eb60efbb27e4d4cbff225bb78c
Eunseo Choi <yses9296@gmail.com>
2022-11-12 18:48:07 +0900

    Slack_Clone react-router  setting

--- alecture/client.tsx
+++ alecture/client.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import App from '@layouts/App';
+import App from '@layouts/App'; //tsconfig로 설정했기 때문에 인식 가능
 
 render(
   <BrowserRouter>
--- alecture/layouts/App.tsx
+++ alecture/layouts/App.tsx
@@ -1,11 +1,22 @@
+import LogIn from '@pages/Login';
+import SignUp from '@pages/SignUp';
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
-  return <div>초기 세팅입니다.</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<LogIn/>}></Route>
+      <Route path="/login" element={<LogIn/>}></Route>
+      <Route path="/signup" element={<SignUp/>}></Route>
+    </Routes>
+  )
+
+
 };
 
 export default App;
 
-//pages
-//components
-//layouts
+//pages - 서비스 페이지
+//components - 페이지 안의 컴포넌트
+//layouts - 공통 레이아웃
